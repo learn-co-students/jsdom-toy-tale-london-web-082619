f065dd3872b7e886a38935fc5fe107ac6e37ae20
github username <github email address>
2019-10-10 16:38:43 +0100

    Done.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const addBtn = document.querySelector('#new-toy-btn');
 const toyForm = document.querySelector('.container');
+const toyFields = toyForm.querySelectorAll(".input-text")
+const createNewToyButton = toyForm.querySelector(".submit")
 const toyCollection = document.querySelector("#toy-collection");
 const path = "http://localhost:3000/toys";
 let addToy = false;
@@ -35,7 +37,7 @@ function renderToys(json) {
 }
 
 function createToyCard(toy) {
-  const card = createBaseCard();
+  const card = createBaseCard(toy.id);
   card.appendChild(createCardHeading(toy.name));
   card.appendChild(createCardImage(toy.image));
   card.appendChild(createCardNumberOfLikes(toy.likes));
@@ -43,8 +45,9 @@ function createToyCard(toy) {
   return card;
 }
 
-function createBaseCard() {
+function createBaseCard(toyId) {
   const card = document.createElement("div");
+  card.id = toyId;
   card.classList.add("card");
   return card;
 }
@@ -64,7 +67,7 @@ function createCardImage(toyImage) {
 
 function createCardNumberOfLikes(toyLikes) {
   const numberOfLikes = document.createElement("p")
-  numberOfLikes.textContent = `${toyLikes} Likes`;
+  numberOfLikes.innerHTML = `<span>${toyLikes}</span> Likes`;
   return numberOfLikes;
 }
 
@@ -72,5 +75,75 @@ function createCardButton() {
   const button = document.createElement("button")
   button.textContent = "Like <3";
   button.classList.add("like-btn");
+  button.addEventListener("click", likeToy);
   return button;
 }
+
+function likeToy(event) {
+  const toyCard = event.target.parentNode;
+  const likeSpan = toyCard.querySelector("span");
+  let likes = parseInt(likeSpan.textContent);
+  likes++;
+  fetch(path + `/${toyCard.id}`, createNewLikeRequestConfig(likes))
+    .then(response => {
+      if (!response.ok) {
+        throw new Error("HTTP status " + reponse.status)
+      }
+      likeSpan.textContent = `${likes}`
+    })
+    .catch(error => alert(error));
+}
+
+createNewToyButton.addEventListener("click", event => createNewToy(event));
+
+function createNewToy(event) {
+  event.preventDefault();
+  if (toyIsValid()) {
+    const newToy = createNewToyObject();
+    postToy(newToy);
+  }
+}
+
+function toyIsValid() {
+  return [...toyFields].every(field => field.value !== "");
+}
+
+function createNewToyObject() {
+  const toy = {}
+  toyFields.forEach(field => {
+    toy[`${field.name}`] = field.value;
+  });
+  toy.likes = 0;
+  return toy;
+}
+
+function postToy(toy) {
+  const requestConfig = createNewToyRequestConfig(toy);
+  return fetch(path, requestConfig)
+    .then(renderToys([toy]))
+    .catch(error => alert(error));
+}
+
+function createNewToyRequestConfig(toy) {
+  const requestConfig = createBasicRequestConfig("POST");
+  requestConfig.body = JSON.stringify(toy);
+  return requestConfig;
+}
+
+function createNewLikeRequestConfig(totalLikes) {
+  const requestConfig = createBasicRequestConfig("PATCH");
+  const likesObj = { likes: totalLikes };
+  requestConfig.body = JSON.stringify(likesObj);
+  return requestConfig;
+}
+
+function createBasicRequestConfig(method) {
+  return {
+    method: `${method}`,
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    }
+  }
+}
+
