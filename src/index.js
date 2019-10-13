037a0acfda7d267bb1961a5a6d5ac554d846b51d
github username <github email address>
2019-10-13 22:09:09 +0100

    chnaged

--- src/index.js
+++ src/index.js
@@ -4,6 +4,63 @@ let addToy = false
 
 // YOUR CODE HERE
 
+
+//1 to get data and convert it into json format
+function fetchGetToys(){
+  return fetch("http://localhost:3000/toys")
+  .then(resp => resp.json())
+  .then(jsonToys => displyJsonToys(jsonToys))
+  
+};
+
+//2 control for iteration
+function displyJsonToys(jsonToys){
+  for (toy of jsonToys) {
+    renderToys(toy);
+  } 
+};
+
+//3 render each item
+function renderToys(toy){
+    const  toyList = document.querySelector("#toy-collection") //格納先
+    // debugger;
+    const div = document.createElement("div");
+    const h2 = document.createElement("h2");
+    const img = document.createElement("img");
+    const p = document.createElement("p");
+    const button = document.createElement("button");
+    div.className = "card";
+    div.id = toy.id //!!!あとでLikeのコントロールを行う際にユニークなidを振っていないとコントロールできない４５行目!!!
+    h2.textContent = toy.name;
+    img.src = toy.image //ソースの時のフォーマット注意！
+    img.className = "toy-avatar" //これを入れていないとイメージが枠内に紐づかず、フォーマっとがぐちゃぐちゃになる！
+    p.textContent = `${toy.likes} Likes`;
+    button.className = "like-btn";
+    button.onclick = (event) => {addLike(event.target.parentElement)}; // got to ４ once triggered.このボタンエレメントでなくエレメント
+    //親であるDiv Elementがターゲットであることに注意
+    div.append(h2,img,p,button); //divのなかに一旦子をひと塊りにして格納
+    toyList.appendChild(div);//格納先に塊として格納
+    return div;//!!!排出！！！
+};// goes bact to 2 until 2 iteration finishes
+
+//4
+function addLike(div){
+  const id = div.id
+  const p = div.querySelector("p")
+  const likes = parseInt(p.textContent.split(' ')[0]) + 1//文字列をArrayに入れて”数字”部分を切り取り文字列を整数に変換しインクリメントができるようにする
+
+  //to save this change in the database
+
+  return fetch(`http://localhost:3000/toys/${id}`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": 'application/json',
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({likes: likes})
+  }).then(p.textContent = `${likes} Likes`) //pageのコンテンツを書き換え
+};
+
 addBtn.addEventListener('click', () => {
   // hide & seek with the form
   addToy = !addToy
@@ -14,5 +71,32 @@ addBtn.addEventListener('click', () => {
   }
 })
 
+toyForm.addEventListener('submit', (e) => {
+  debugger;
+  e.preventDefault();
+  const toy = {
+    name: e.target[0].value,
+    image: e.target[1].value,
+    likes: 0
+  }
+  fetchPostToy(toy) // データベースに書き込み
+  .then(resp => resp.json())//その後画面に出力するためデータをデータベースより受け取り開始
+  .then(jsonToy => renderToys(jsonToy))//
+})
+
+function fetchPostToy(toy) {
+  return fetch("http://localhost:3000/toys", {
+    method: 'POST',
+    headers: {
+      "Content-Type": "application/jso",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(toy)
+  })
+}
 
 // OR HERE!
+
+window.addEventListener("DOMContentLoaded", (event) => {
+  fetchGetToys();// to triger "get" json. go to 1
+})
